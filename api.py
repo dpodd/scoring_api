ff34c23c2a20991e7b9083fdcc884b36d25727ac
dpodd <fuzzyworld15@gmail.com>
2021-01-09 17:22:24 +0700

    Submit homework #3

--- api.py
+++ api.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import abc
+from abc import ABC
 import json
 import datetime
 import logging
 import hashlib
 import uuid
 from optparse import OptionParser
-try:
-    from http.server import HTTPServer, BaseHTTPRequestHandler    
-except:
-    from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from typing import Type
+import re
+from scoring import get_score, get_interests
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 SALT = "Otus"
 ADMIN_LOGIN = "admin"
@@ -37,106 +37,199 @@ GENDERS = {
     MALE: "male",
     FEMALE: "female",
 }
+AGE_LIMIT = 70
+
 
 class ValidationError(Exception):
     """Raises when validation check fails"""
     def __init__(self, message=None):
         self.message = message
 
-class CharField(object):
+
+class BaseField(ABC):
     def __init__(self, required=False, nullable=True):
         self.required = required
         self.nullable = nullable
-    
+        self._value = None
+
     def __get__(self, instance, owner):
         return self._value
-    
-    def __set__(self, instance, value):        
+
+
+class CharField(BaseField):
+    def __set__(self, instance, value):
+        if value is None:
+            if self.required is True:
+                raise ValidationError(message="The parameter is mandatory")
+            
+            if self.nullable is False:
+                raise ValidationError(message="The parameter should be non-nullable")
+
+            self._value = value
+        else:
+            if not isinstance(value, str):
+                raise ValidationError(message="The parameter should be a string")
+
+            self._value = value
+
+
+class ArgumentsField(BaseField):
+    def __set__(self, instance, value):
         if value is None:
             if self.required is True:
                 raise ValidationError(message="The parameter is mandatory")
             
             if self.nullable is False:
                 raise ValidationError(message="The parameter should be non-nullable")        
-            else:
-                self._value = ''            
+
+            self._value = value
         else:
+            if not isinstance(value, dict):
+                raise ValidationError(message="The parameter should be a dictionary")
+        
+            self._value = value
+
+
+class EmailField(BaseField):
+    def __set__(self, instance, value):
+        if value is None:
+            if self.required is True:
+                raise ValidationError(message="The parameter is mandatory")
+
+            if self.nullable is False:
+                raise ValidationError(message="The parameter should be non-nullable")
+
+            self._value = value
+        else:
+            if not isinstance(value, str):
+                raise ValidationError(message="The parameter should be a string")
+
+            if '@' not in value:
+                raise ValidationError(message="Invalid email given")
+
+            self._value = value
+
+
+class PhoneField(BaseField):
+    def __set__(self, instance, value):
+        if value is None:
+            if self.required is True:
+                raise ValidationError(message="The parameter is mandatory")
+
+            if self.nullable is False:
+                raise ValidationError(message="The parameter should be non-nullable")
+
+            self._value = value
+        else:
+            if not isinstance(value, (int, str)):
+                raise ValidationError(message="The parameter should be a string or integer")
+
+            phone_pattern = re.compile(r'^7\d{10}')
+            if not phone_pattern.match('%s' % value):
+                raise ValidationError(message="Invalid phone number")
+
+            self._value = value
+
+
+class DateField(BaseField):
+    def __set__(self, instance, value):
+        if value is None:
+            if self.required is True:
+                raise ValidationError(message="The parameter is mandatory")
+
+            if self.nullable is False:
+                raise ValidationError(message="The parameter should be non-nullable")
+
+            self._value = value
+        else:
+            if not isinstance(value, str):
+                raise ValidationError(message="The parameter should be a string")
+
+            date_pattern = re.compile(r'(\d{2})\.(\d{2})\.(\d{4})')
+            match = date_pattern.match(value)
+            if match:
+                day = int(match.group(1))
+                month = int(match.group(2))
+                year = int(match.group(3))
+            else:
+                raise ValidationError(message="Invalid date; date should have a format DD.MM.YYYY")
+
+            self._value = datetime.date(year=year, month=month, day=day)
+
+
+class BirthDayField(BaseField):
+    def __set__(self, instance, value):
+        if value is None:
+            if self.required is True:
+                raise ValidationError(message="The parameter is mandatory")
+
+            if self.nullable is False:
+                raise ValidationError(message="The parameter should be non-nullable")
+
+            self._value = value
+        else:
+            if not isinstance(value, str):
+                raise ValidationError(message="The parameter should be a string")
+
+            birthday_pattern = re.compile(r'(\d{2})\.(\d{2})\.(\d{4})')
+            match = birthday_pattern.match(value)
+            if match:
+                day = int(match.group(1))
+                month = int(match.group(2))
+                year = int(match.group(3))
+            else:
+                raise ValidationError(message="Invalid date; date should have a format DD.MM.YYYY")
+
+            birthday = datetime.date(year=year, month=month, day=day)
+
+            if datetime.date.today() - birthday > datetime.timedelta(AGE_LIMIT * 365.2425):
+                raise ValidationError(message="Age limit exceeded")
+
+            self._value = birthday
+
+
+class GenderField(BaseField):
+    def __set__(self, instance, value):
+        if value is None:
+            if self.required is True:
+                raise ValidationError(message="The parameter is mandatory")
+
+            if self.nullable is False:
+                raise ValidationError(message="The parameter should be non-nullable")
+
+            self._value = value
+        else:
+            if not isinstance(value, int):
+                raise ValidationError(message="The parameter should be an integer")
+
+            if value not in [0, 1, 2]:
+                raise ValidationError(message="Invalid gender field")
+
             self._value = str(value)
 
 
-class ArgumentsField(object):
-    def __init__(self, required=False, nullable=True):
-        self.required = required
-        self.nullable = nullable
-    
-    def __get__(self, instance, owner):
-        return self._value
-    
-    def __set__(self, instance, value):  
-        print('arguments __set__:', instance, value)      
+class ClientIDsField(BaseField):
+    def __set__(self, instance, value):
         if value is None:
             if self.required is True:
                 raise ValidationError(message="The parameter is mandatory")
-            
+
             if self.nullable is False:
-                raise ValidationError(message="The parameter should be non-nullable")        
-            else:
-                self._value = {}           
-        
-        if not isinstance(value, dict):
-            raise ValidationError(message="The parameter should be a dictionary")
-        
-        self._value = value
+                raise ValidationError(message="The parameter should be non-nullable")
 
+            self._value = value
+        else:
+            if not isinstance(value, list):
+                raise ValidationError(message="The parameter should be a list")
 
-class EmailField(CharField):
-    pass
+            if not value:
+                raise ValidationError(message="Client ID list is empty")
 
+            for item in value:
+                if not isinstance(item, int):
+                    raise ValidationError(message="Invalid client ID is given")
 
-class PhoneField(object):
-    def __init__(self, required=None, nullable=None):
-        self.required = required
-        self.nullable = nullable
-
-
-class DateField(object):
-    def __init__(self, required=None, nullable=None):
-        self.required = required
-        self.nullable = nullable
-
-
-class BirthDayField(object):
-    def __init__(self, required=None, nullable=None):
-        self.required = required
-        self.nullable = nullable
-
-
-class GenderField(object):
-    def __init__(self, required=None, nullable=None):
-        self.required = required
-        self.nullable = nullable
-
-
-class ClientIDsField(object):
-    def __init__(self, required=None):
-        self.required = required
-
-    def __get__(self, instance, owner):
-        pass
-
-
-class ClientsInterestsRequest(object):
-    client_ids = ClientIDsField(required=True)
-    date = DateField(required=False, nullable=True)
-
-
-class OnlineScoreRequest(object):
-    first_name = CharField(required=False, nullable=True)
-    last_name = CharField(required=False, nullable=True)
-    email = EmailField(required=False, nullable=True)
-    phone = PhoneField(required=False, nullable=True)
-    birthday = BirthDayField(required=False, nullable=True)
-    gender = GenderField(required=False, nullable=True)
+            self._value = value
 
 
 class MethodRequest(object):
@@ -150,47 +243,105 @@ class MethodRequest(object):
     def is_admin(self):
         return self.login == ADMIN_LOGIN
 
-    @classmethod
-    def validate(cls, request):
-        code = OK
-        try:
-            cls.account = request.get('body').get('account')
-            cls.login = request.get('body').get('login')
-            cls.token = request.get('body').get('token')
-            cls.arguments = request.get('body').get('arguments')
-            cls.method = request.get('body').get('method')
-            print('-'*20)
-            print(cls.account, cls.login, cls.token, type(cls.arguments), cls.method)
-        except ValidationError as e:
-            code = INVALID_REQUEST
-            # add logging
-        return code
-        
+    def validate(self, request):
+        self.account = request.get('body').get('account')
+        self.login = request.get('body').get('login')
+        self.token = request.get('body').get('token')
+        self.arguments = request.get('body').get('arguments')
+        self.method = request.get('body').get('method')
+        return self
 
+
+class ClientsInterestsRequest(MethodRequest):
+    client_ids = ClientIDsField(required=True)
+    date = DateField(required=False, nullable=True)
+
+    def validate(self, request: Type[MethodRequest]):
+        self.client_ids = request.arguments.get('client_ids')
+        self.date = request.arguments.get('date')
+        return self
+
+    def get_interests(self):
+        store = {"nclients": 0}
+        result = {}
+        for cid in self.client_ids:
+            interests, store = get_interests(store, cid)
+            result.update({str(cid): interests})
+        return result, store
+
+
+class OnlineScoreRequest(MethodRequest):
+    first_name = CharField(required=False, nullable=True)
+    last_name = CharField(required=False, nullable=True)
+    email = EmailField(required=False, nullable=True)
+    phone = PhoneField(required=False, nullable=True)
+    birthday = BirthDayField(required=False, nullable=True)
+    gender = GenderField(required=False, nullable=True)
+    VALID_ARGUMENTS = ["first_name", "last_name", "email", "phone", "birthday", "gender"]
+
+    def validate(self, request: Type[MethodRequest]):
+        self.first_name = request.arguments.get('first_name')
+        self.last_name = request.arguments.get('last_name')
+        self.email = request.arguments.get('email')
+        self.phone = request.arguments.get('phone')
+        self.birthday = request.arguments.get('birthday')
+        self.gender = request.arguments.get('gender')
+
+        if not ((self.phone and self.email) or (self.first_name and self.last_name) or (self.gender and self.birthday)):
+            raise ValidationError(message="Not enough data provided")
+
+        return self
+
+    def get_score(self):
+        return get_score(None, self.phone, self.email, birthday=self.birthday,
+                         gender=self.gender, first_name=self.first_name, last_name=self.last_name)
 
 
 def check_auth(request):
     if request.is_admin:
-        digest = hashlib.sha512(datetime.datetime.now().strftime("%Y%m%d%H") + ADMIN_SALT).hexdigest()
+        phrase = datetime.datetime.now().strftime("%Y%m%d%H") + ADMIN_SALT
+        digest = hashlib.sha512(phrase.encode('utf-8')).hexdigest()
     else:
-        digest = hashlib.sha512(request.account + request.login + SALT).hexdigest()
+        phrase = request.account + request.login + SALT
+        digest = hashlib.sha512(phrase.encode('utf-8')).hexdigest()
     if digest == request.token:
         return True
     return False
 
 
 def method_handler(request, ctx, store):
-    response, code = None, None
-    # 1 valid general request structure
+    response, code, ctx = None, None, ctx
     try:
-        code = MethodRequest.validate(request)
-        print("code: ", code)
-    except:
-        pass
-    # 2 check auth
-    # 3 valid specific request
-    response = "placeholder"
-    return response, code
+        # 1 validate general request structure
+        request = MethodRequest().validate(request)
+
+        # 2 check auth
+        if not check_auth(request):
+            logging.error("Wrong authentication token")
+            return None, FORBIDDEN, ctx
+
+        # 3 validate method arguments and dispatch to a specific method
+        if request.method == 'online_score':
+            # adding args to the context dictionary
+            ctx['has'] = [key for key in request.arguments.keys() if key in OnlineScoreRequest.VALID_ARGUMENTS]
+            # process a request
+            request = OnlineScoreRequest().validate(request)
+            if not request.is_admin:
+                score = request.get_score()
+            else:
+                score = 42
+            response = {"score": score}
+            return response, OK, ctx
+
+        elif request.method == 'clients_interests':
+            request = ClientsInterestsRequest().validate(request)
+            response, store = request.get_interests()
+            ctx.update(store)
+            return response, OK, ctx
+    except ValidationError as e:
+        logging.error("Validation error: %s" % e.message)
+        code = INVALID_REQUEST
+        return {'msg': 'validation error'}, code, ctx
 
 
 class MainHTTPHandler(BaseHTTPRequestHandler):
@@ -217,13 +368,13 @@ class MainHTTPHandler(BaseHTTPRequestHandler):
             logging.info("%s: %s %s" % (self.path, data_string, context["request_id"]))
             if path in self.router:
                 try:
-                    response, code = self.router[path]({"body": request, "headers": self.headers}, context, self.store)
+                    response, code, context = self.router[path]({"body": request, "headers": self.headers},
+                                                                context, self.store)
                 except Exception as e:
                     logging.exception("Unexpected error: %s" % e)
                     code = INTERNAL_ERROR
             else:
                 code = NOT_FOUND
-
         self.send_response(code)
         self.send_header("Content-Type", "application/json")
         self.end_headers()
@@ -233,13 +384,14 @@ class MainHTTPHandler(BaseHTTPRequestHandler):
             r = {"error": response or ERRORS.get(code, "Unknown Error"), "code": code}
         context.update(r)
         logging.info(context)
-        self.wfile.write(json.dumps(r))
+        result_string = json.dumps(r).encode('utf-8')
+        self.wfile.write(result_string)
         return
 
 
 if __name__ == "__main__":
     op = OptionParser()
-    op.add_option("-p", "--port", action="store", type=int, default=8080)
+    op.add_option("-p", "--port", action="store", type=int, default=8089)
     op.add_option("-l", "--log", action="store", default=None)
     (opts, args) = op.parse_args()
     logging.basicConfig(filename=opts.log, level=logging.INFO,
--- test.py
+++ test.py
@@ -28,13 +28,14 @@ class TestSuite(unittest.TestCase):
 
     def set_valid_auth(self, request):
         if request.get("login") == api.ADMIN_LOGIN:
-            request["token"] = hashlib.sha512(datetime.datetime.now().strftime("%Y%m%d%H") + api.ADMIN_SALT).hexdigest()
+            msg = datetime.datetime.now().strftime("%Y%m%d%H") + api.ADMIN_SALT
+            request["token"] = hashlib.sha512(msg.encode('utf-8')).hexdigest()
         else:
             msg = request.get("account", "") + request.get("login", "") + api.SALT
-            request["token"] = hashlib.sha512(msg).hexdigest()
+            request["token"] = hashlib.sha512(msg.encode('utf-8')).hexdigest()
 
     def test_empty_request(self):
-        _, code = self.get_response({})
+        _, code, ctx = self.get_response({})
         self.assertEqual(api.INVALID_REQUEST, code)
 
     @cases([
@@ -43,7 +44,7 @@ class TestSuite(unittest.TestCase):
         {"account": "horns&hoofs", "login": "admin", "method": "online_score", "token": "", "arguments": {}},
     ])
     def test_bad_auth(self, request):
-        _, code = self.get_response(request)
+        _, code, ctx = self.get_response(request)
         self.assertEqual(api.FORBIDDEN, code)
 
     @cases([
@@ -53,7 +54,7 @@ class TestSuite(unittest.TestCase):
     ])
     def test_invalid_method_request(self, request):
         self.set_valid_auth(request)
-        response, code = self.get_response(request)
+        response, code, ctx = self.get_response(request)
         self.assertEqual(api.INVALID_REQUEST, code)
         self.assertTrue(len(response))
 
@@ -75,7 +76,7 @@ class TestSuite(unittest.TestCase):
     def test_invalid_score_request(self, arguments):
         request = {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "arguments": arguments}
         self.set_valid_auth(request)
-        response, code = self.get_response(request)
+        response, code, ctx = self.get_response(request)
         self.assertEqual(api.INVALID_REQUEST, code, arguments)
         self.assertTrue(len(response))
 
@@ -92,7 +93,7 @@ class TestSuite(unittest.TestCase):
     def test_ok_score_request(self, arguments):
         request = {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "arguments": arguments}
         self.set_valid_auth(request)
-        response, code = self.get_response(request)
+        response, code, ctx = self.get_response(request)
         self.assertEqual(api.OK, code, arguments)
         score = response.get("score")
         self.assertTrue(isinstance(score, (int, float)) and score >= 0, arguments)
@@ -102,7 +103,7 @@ class TestSuite(unittest.TestCase):
         arguments = {"phone": "79175002040", "email": "stupnikov@otus.ru"}
         request = {"account": "horns&hoofs", "login": "admin", "method": "online_score", "arguments": arguments}
         self.set_valid_auth(request)
-        response, code = self.get_response(request)
+        response, code, ctx = self.get_response(request)
         self.assertEqual(api.OK, code)
         score = response.get("score")
         self.assertEqual(score, 42)
@@ -118,7 +119,7 @@ class TestSuite(unittest.TestCase):
     def test_invalid_interests_request(self, arguments):
         request = {"account": "horns&hoofs", "login": "h&f", "method": "clients_interests", "arguments": arguments}
         self.set_valid_auth(request)
-        response, code = self.get_response(request)
+        response, code, ctx = self.get_response(request)
         self.assertEqual(api.INVALID_REQUEST, code, arguments)
         self.assertTrue(len(response))
 
@@ -130,10 +131,10 @@ class TestSuite(unittest.TestCase):
     def test_ok_interests_request(self, arguments):
         request = {"account": "horns&hoofs", "login": "h&f", "method": "clients_interests", "arguments": arguments}
         self.set_valid_auth(request)
-        response, code = self.get_response(request)
+        response, code, ctx = self.get_response(request)
         self.assertEqual(api.OK, code, arguments)
         self.assertEqual(len(arguments["client_ids"]), len(response))
-        self.assertTrue(all(v and isinstance(v, list) and all(isinstance(i, basestring) for i in v)
+        self.assertTrue(all(v and isinstance(v, list) and all(isinstance(i, str) for i in v)
                         for v in response.values()))
         self.assertEqual(self.context.get("nclients"), len(arguments["client_ids"]))
 
