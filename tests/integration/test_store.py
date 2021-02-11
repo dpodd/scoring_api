aac62d2660cb5ac1c41b6cfcbcaa76ea37da8e5d
dpodd <fuzzyworld15@gmail.com>
2021-02-11 11:07:43 +0700

    Add StoreDisconnectedTestCase

--- tests/integration/test_store.py
+++ tests/integration/test_store.py
@@ -1,5 +1,8 @@
 import unittest
 
+import fakeredis
+import redis
+
 from store import Store
 
 
@@ -40,6 +43,36 @@ class StoreConnectedTestCase(unittest.TestCase):
         self.keys_for_cleanup.append(t[0])
         self.assertEqual(self.store.cache_set(*t, 60*10), True)
         self.assertEqual(self.store.cache_get(t[0]).decode(), t[1])
+
+
+class StoreDisconnectedTestCase(unittest.TestCase):
+    """ Tests should pass when our key-value storage (and cache) are not available """
+    def setUp(self):
+        self.store = Store()
+        # emulate dysfunctional storage:
+        server = fakeredis.FakeServer()
+        server.connected = False
+        self.store.client = fakeredis.FakeStrictRedis(server=server)
+
+    def test_our_mock_redis(self):
+        self.assertRaises(redis.exceptions.ConnectionError, self.store.ping)
+
+    def test_store_set_value(self):
+        t = ("test_key", "test_value")
+        self.assertRaises(redis.exceptions.ConnectionError, self.store.set, *t)
+
+    def test_store_get_value(self):
+        t = ("test_key", "test_value")
+        self.assertRaises(redis.exceptions.ConnectionError, self.store.get, t[0])
+
+    def test_store_set_cache_value(self):
+        t = ("test_key", "test_value")
+        self.assertRaises(redis.exceptions.ConnectionError, self.store.cache_set, *t, 60)
+
+    def test_store_get_cache_value(self):
+        """ Cache returns None when storage is disconnected"""
+        t = ("test_key", "test_value")
+        self.assertEqual(self.store.cache_get(t[0]), None)
 
 
 if __name__ == '__main___':
